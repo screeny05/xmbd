21a0f2dcc4ab46434c00877638d4d8dcb6f6f4b1
Screeny <screeny05@gmail.com>
2013-10-14 18:25:32 +0200

    pretty much everything commented

--- xmbd.jquery.js
+++ xmbd.jquery.js
@@ -6,12 +6,16 @@
 	$.fn.xmbd = function(){
 		var $plg = this;
 		
+		// Always be sure to update these variables!
 		$plg.current = {
 			state: -1,
 			provider: "",
 			id: ""
 		};
 		
+		// And be sure to never change these.
+		// Except you want to rename one, but be sure
+		// that there will be no one left behind.
 		$plg.availableStates = {
 			"-1": "vUnstarted",
 			0: "vEnded",
@@ -22,11 +26,17 @@
 			9: "playerReady",
 		};
 		
-		// Generate a GUID to use as the players id
+		// Generate a GUID to use as the players id.
+		// This also ensures we always get the right events for our player.
+		// Take a look at the very last lines of code
+		// which add methods like `onYouTubePlayerReady` to the window object.
 		$plg.guid = $plg.guid="xxxxxxxx_xxxx_4xxx_yxxx_xxxxxxxxxxxx".replace(/[xy]/g,function(b){var a=16*Math.random()|0;return("x"==b?a:a&3|8).toString(16);});
 		
 		/*
 		*	Small Library for triggering Events
+		*	*****
+		*	To trigger one, either use `$plg.trigger` or the more convenient
+		*	`$plg.triggerStateChange` for StateChanges
 		*/
 		var events = {};
 		$plg.on = function(e, fn){
@@ -44,16 +54,18 @@
 		/*
 		*	Media Provider Library
 		*	Providers need:
-		*	- iframe<bool> true = url is an iframe, not a swf
-		*	- getId(url) [turns a full url into the video-id]
-		*	- getUrl(id, options) [turns a video-id with given options into a embeddable url]
-		*	- state(s) [turns the state from the player into a yt-api-compatible state-number]
-		*	- play, pause, toggle, stop, cue [should explain themselves]
+		*	- iframe:bool true = url is an iframe, not a swf
+		*	- getId(url:string) - turns a full url into the video-id
+		*	- getUrl(id:string, options:object) - turns a video-id with given options into a embeddable url
+		*	- state(s:var) - turns the state of the player into a yt-api-compatible state-number
+		*	- play, pause, stop, cue - should explain themselves
 		*/
 		$plg.provider = {
 			youtube: {
+				// we use YouTube's ActionScript-API
 				iframe: false,
 				getId: function(url){
+					// Found this RegEx somewhere on SO; Maybe i can give credit to the creator when i find it again.
 					var m = url.match(/(?:youtube(?:-nocookie)?\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i);
 					if(!m)
 						return false;
@@ -61,6 +73,12 @@
 						return m[1];
 				},
 				getUrl: function(id, options){
+					// Note: (+(true)) turns to to a number-type. i.e: 1.
+					// JSHint-Compatible!
+					// For a detailed explanation of parameters,
+					// see: https://developers.google.com/youtube/player_parameters
+					
+					// be sure to have a options-object!
 					options = options || {};
 					var params = [
 						"?autoplay="	+ (+(options.autoplay || false)),
@@ -73,16 +91,22 @@
 						"version=3",
 						"rel=0"
 					].join("&");
+					// Yeah, I hear you saying:
+					// > string concatenation like this is not the fastest way, just use the +-operator!
+					// No, I won't. it's just more convenient imho. (more info: http://bit.ly/1cOocNZ)
 				
 					return "//www.youtube.com/v/" + id + params;
 				},
 				state: function(s){
+					// If the youtube-numbered-state is in the list of available states
+					// emit it. else return whatever status it may be.
 					if($plg.availableStates.hasOwnProperty(s)){
 						return $plg.availableStates[s];
 					} else {
 						return s;
 					}
 				},
+				// Just some proxy-functions
 				play: function(){
 					$plg.player.playVideo();
 				},
@@ -106,6 +130,9 @@
 						return m[1];
 				},
 				getUrl: function(id, options){
+					// For a detailed explanation of the parameters
+					// see: http://developer.vimeo.com/player/embedding#universal-parameters
+					options = options || {};
 					var params = [
 						"?autoplay="	+ (+(options.autoplay || false)),
 						"byline="		+ (+(options.autohide || false)),
@@ -130,8 +157,17 @@
 					}
 				},
 				post: function(method, value){
+					// vimeo uses a different kind of API than YouTube
+					// because it uses an Iframe and EventListeners
+					// so we have to "post" messages to the iframe
+					// see: http://developer.vimeo.com/player/js-api#universal-with-postmessage
+					
 					value = value || true;
 					var p = $("#" + $plg.guid);
+					
+					// NEVER forget to include the protocol!
+					// That's because we use protocol-relative urls.
+					// see: http://www.paulirish.com/2010/the-protocol-relative-url/
 					var url = window.location.protocol + p.attr("src").split("?")[0];
 					p[0].contentWindow.postMessage(JSON.stringify({ method: method, value: value }), url);
 				},
@@ -151,6 +187,10 @@
 			dailymotion: {
 				iframe: false,
 				getId: function(url){
+					// Dailymotion URLs may be a little weird.
+					// if m[2] is not null we use this instead of m[1]
+					// that's because the url may contain a video in the hashtag.
+					// if there's no video-id in the hashtag we can safely use m[1] (the original id)
 					var m = url.match(/dailymotion.com\/(?:(?:video|hub)\/([^_]+))?[^#]*(?:#video=([^_&]+))?/i);
 					if(!m)
 						return false;
@@ -161,6 +201,9 @@
 							return m[1];
 				},
 				getUrl: function(id, options){
+					// For a detailed explanation of the params,
+					// see: http://www.dailymotion.com/doc/api/player.html#parameters
+					options = options || {};
 					var params = [
 						"?autoPlay="	+ (options.autoplay || false),
 						(options.highlight === false ? "" : "highlight="	+ options.highlight),
@@ -186,6 +229,9 @@
 			}
 		};
 		
+		// Abstracted Function to provide easier access to all
+		// available media-providers.
+		// Executes the getUrl()-method of the given provider or returns an error.
 		$plg.getMediaUrl = function(prov, id, options){
 			if(!$plg.provider.hasOwnProperty(prov)){
 				$plg.trigger("error", "Provider not supported");
@@ -196,6 +242,11 @@
 				iframe: $plg.provider[prov].iframe
 			};
 		};
+		
+		// Abstracted Function (see $plg.getMediaUrl)
+		// Executes the getId()-method of each provider to test
+		// if the url is a valid url of the current provider
+		// and to return the media-id and iframe-property
 		$plg.getMediaId = function(url){
 			for(var provider in $plg.provider){
 				if($plg.provider.hasOwnProperty(provider)){
@@ -212,54 +263,11 @@
 			return false;
 		};
 		
-		$plg.embed = function(prov, id, options){
-			if(typeof prov == "object" && typeof id == "undefined" && typeof options == "undefined"){
-				if(prov.hasOwnProperty("options"))
-					options = prov.options;
-				if(prov.hasOwnProperty("on"))
-					options.on = prov.on;
-				id = prov.id;
-				prov = prov.provider;
-			}
-			options = options || {};
-			if(options.hasOwnProperty("on")){
-				for(var key in options.on){
-					if(options.on.hasOwnProperty(key)){
-						$plg.on(key, options.on[key]);
-					}
-				}
-			}
-			
-			var embedObj = $plg.getMediaUrl(prov, id, options);
-			$plg.current.provider = prov;
-			$plg.current.id = id;
-			
-			if($("#" + $plg.guid).length > 0)
-				$("#" + $plg.guid).remove();
-			
-			if($plg.player)
-				$plg.triggerStateChange($plg.availableStates["-1"]);
-			
-			if(!embedObj.iframe)
-				$plg.embedSWF(embedObj.url);
-			else
-				$plg.embedIframe(embedObj.url);
-		};
-		
-		$plg.embedIframe = function(url){
-			$plg.append("<iframe src='" + url + "' id='" + $plg.guid + "' width='100%' height='100%' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>");
-		};
-		
-		$plg.embedSWF = function(url){
-			$plg.html("<div id='" + $plg.guid + "'></div>");
-			swfobject.embedSWF(url, $plg.guid, "100%", "100%", "8", null, null, { allowScriptAccess: "always", allowFullScreen: "true" }, { id: $plg.guid }, function(e){
-				if(!e.success){
-					$plg.trigger("error", "Unable to embed SWF");
-					throw new Error("Unable to embed SWF");
-				}
-			});
-		};
-		
+		// Abstracted Function (see $plg.getMediaUrl)
+		// if the provider offers a method such as `play`
+		// execute it. else check if the user wants to
+		// toggle (play on paused, pause on playing)
+		// if neither is the case, throw an error.
 		$plg.action = function(s){
 			if($plg.provider[$plg.current.provider].hasOwnProperty(s)){
 				return $plg.provider[$plg.current.provider][s].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -274,24 +282,93 @@
 			}
 		};
 		
+		// Easy-Access function to embed media.
+		// Look at the documentation for how to use.
+		// Look at the comments for how to modify.
+		$plg.embed = function(prov, id, options){
+			// if we use the object-style version, we have to
+			// provide the right variables to the code.
+			// i.e: prov from options, id from options.
+			if(typeof prov == "object" && typeof id == "undefined" && typeof options == "undefined"){
+				options = prov;
+				id = options.id;
+				prov = options.provider;
+			}
+			
+			// Add all the event-handlers in the on-object.
+			if(options.hasOwnProperty("on")){
+				for(var key in options.on){
+					if(options.on.hasOwnProperty(key)){
+						$plg.on(key, options.on[key]);
+					}
+				}
+			}
+			
+			// retrieve embed url and iframe-bool from our abstracted method.
+			var embedObj = $plg.getMediaUrl(prov, id, options);
+			
+			// be sure to update these!
+			$plg.current.provider = prov;
+			$plg.current.id = id;
+			
+			// if there already is an embedded object, delete it
+			// so we don't have two instances.
+			if($("#" + $plg.guid).length > 0)
+				$("#" + $plg.guid).remove();
+			
+			if($plg.player)
+				$plg.triggerStateChange($plg.availableStates["-1"]);
+			
+			// embed the object according to it's iframe-attribute
+			if(!embedObj.iframe)
+				$plg.embedSWF(embedObj.url);
+			else
+				$plg.embedIframe(embedObj.url);
+		};
+		
+		$plg.embedIframe = function(url){
+			$plg.append("<iframe src='" + url + "' id='" + $plg.guid + "' width='100%' height='100%' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>");
+		};
+		
+		$plg.embedSWF = function(url){
+			$plg.html("<div id='" + $plg.guid + "'></div>");
+			swfobject.embedSWF(url, $plg.guid, "100%", "100%", "8", null, null, { allowScriptAccess: "always", allowFullScreen: "true" }, { id: $plg.guid }, function(e){
+				// throw an error if we had no success embedding the swfobject.
+				if(!e.success){
+					$plg.trigger("error", "Unable to embed SWF");
+					throw new Error("Unable to embed SWF");
+				}
+			});
+		};
+		
+		// Updates the State and throws necessary Events.
 		$plg.triggerStateChange = function(s){
 			$plg.trigger(s);
 			$plg.trigger("playerStateChange", s);
 			$plg.current.state = s;
 		};
 		
-		// Youtube-Compatible
+		// Youtube-Compatible Media-Providers use this.
+		// i.e: YouTube, DailyMotion
 		window["onytcstatechange_" + $plg.guid] = function(s){
 			s = $plg.provider.youtube.state(s);
 			$plg.triggerStateChange(s);
+			
+			// if this is the inital throw of this event
+			// change the $plg. player property, so we can
+			// execute player-specific methods like play & pause.
 			if(s == "playerReady"){
 				$plg.player = document.getElementById($plg.guid);
 			}
 		};
-		// Vimeo
+		// Vimeo stateChange-Handler
 		window["onvimeostatechange_" + $plg.guid] = function(s){
 			s = $plg.provider.vimeo.state(s);
 			$plg.triggerStateChange(s);
+			
+			// if this is the inital throw of this event
+			// add eventListeners for all necessary events.
+			// TODO: are Errors also appended?
 			if(s == "playerReady"){
 				$(["pause", "finish", "play"]).each(function(i, e){
 					$plg.provider.vimeo.post("addEventListener", e);
@@ -316,6 +393,7 @@ window.onVimeoMessage = function(m){
 	var data = JSON.parse(m.data);
 	window["onvimeostatechange_" + data.player_id](data.event);
 };
+// see Vimeo API-Docs for more info about this.
 if(window.addEventListener)
 	window.addEventListener('message', window.onVimeoMessage, false);
 else
